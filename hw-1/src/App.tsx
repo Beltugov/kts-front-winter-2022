a214ed812fc3b520928c9dad61a794101e7efa83
Beltugov <beltugovsam@gmail.com>
2022-02-05 19:40:11 +0300

    hw-1

---
+++ hw-1/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import root from './root/root';
+
+function App() {
+    root();
+    return (
+        <div className="App">
+        </div>
+    );
+}
+
+export default App;
