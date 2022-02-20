cd1e10db88de49c4bb7759a6053e946290c0fbaa
Beltugov <beltugovsam@gmail.com>
2022-02-20 18:47:44 +0300

    Beltugov
    Add hw-2

---
+++ hw-2/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import ReposSearchPage from "./pages/ReposSearchPage";
+
+import "./App.css";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ReposSearchPage />
+    </div>
+  );
+}
+
+export default App;
